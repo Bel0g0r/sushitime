5dbd9d76ef35733a12e59bc68e7ecb4b8ae25e6e
Bel0g0r <46893238+Bel0g0r@users.noreply.github.com>
2020-01-09 01:58:30 +0300

    Загрузка проекта, попытка 2

---
+++ precache-manifest.10b8b3c2952274c98c9ed540342dbbbe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d49d040429ae320d02bcce4d59b38790",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4143be2c54ff67f77eab",
+    "url": "/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "f8491c669f91386aa0a0",
+    "url": "/static/css/main.33eb472a.chunk.css"
+  },
+  {
+    "revision": "4143be2c54ff67f77eab",
+    "url": "/static/js/2.67326c0e.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.67326c0e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f8491c669f91386aa0a0",
+    "url": "/static/js/main.4eadf734.chunk.js"
+  },
+  {
+    "revision": "236d690b4d17eeb71dfd",
+    "url": "/static/js/runtime-main.66f76688.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
