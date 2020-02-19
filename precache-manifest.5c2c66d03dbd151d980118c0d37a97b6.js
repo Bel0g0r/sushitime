9bd17272ebf1495982d5caedb4c3aee0746dd8a2
Bel0g0r <46893238+Bel0g0r@users.noreply.github.com>
2020-02-20 02:17:56 +0300

    Версия от 20 февраля - уведомления

---
+++ precache-manifest.5c2c66d03dbd151d980118c0d37a97b6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bcbfb697bac516a247a2db60efc565d",
+    "url": "/sushitime/index.html"
+  },
+  {
+    "revision": "dabde8aca5c9804c15c5",
+    "url": "/sushitime/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "f8659d50e5876909ec09",
+    "url": "/sushitime/static/css/main.9eac0247.chunk.css"
+  },
+  {
+    "revision": "dabde8aca5c9804c15c5",
+    "url": "/sushitime/static/js/2.331625dd.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/sushitime/static/js/2.331625dd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f8659d50e5876909ec09",
+    "url": "/sushitime/static/js/main.17acc3cb.chunk.js"
+  },
+  {
+    "revision": "708856230e5303eebd03",
+    "url": "/sushitime/static/js/runtime-main.6ddae756.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/sushitime/static/media/persik.4e1ec840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sushitime/precache-manifest.cb261da0f1855d5143b36a22530bcc4a.js"
+  "/sushitime/precache-manifest.5c2c66d03dbd151d980118c0d37a97b6.js"
 );
 
 self.addEventListener('message', (event) => {
