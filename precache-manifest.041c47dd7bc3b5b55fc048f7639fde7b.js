0cbe637bfd2c9c978283f579cdeac8aafe3f500c
Bel0g0r <46893238+Bel0g0r@users.noreply.github.com>
2020-04-22 04:44:58 +0300

    Тестовая версия - с целью "Заказ"

---
+++ precache-manifest.041c47dd7bc3b5b55fc048f7639fde7b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c848fec49eae725ea12f5d00c146219",
+    "url": "/sushitime/index.html"
+  },
+  {
+    "revision": "a86ab439b9b6b5a5848b",
+    "url": "/sushitime/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "da3f3d09cd1f8a512edb",
+    "url": "/sushitime/static/css/main.28b852b5.chunk.css"
+  },
+  {
+    "revision": "a86ab439b9b6b5a5848b",
+    "url": "/sushitime/static/js/2.14399b5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sushitime/static/js/2.14399b5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da3f3d09cd1f8a512edb",
+    "url": "/sushitime/static/js/main.3757a8b2.chunk.js"
+  },
+  {
+    "revision": "1e888aebbe52856a82d4",
+    "url": "/sushitime/static/js/runtime-main.a1cde225.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/sushitime/static/media/persik.4e1ec840.png"
+  }
+]);
